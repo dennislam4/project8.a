27d760ffde0465bda277ebcaf7885a196da6c6c6
dennis lam <lamden@oregonstate.edu>
2022-07-20 14:18:13 -0600

    Initial commit

---
+++ count_letters.py
@@ -0,0 +1,17 @@
+# Author: Dennis Lam
+# GitHub username: dennislam4
+# Date: 7/13/2022
+# Description: A function that takes as a parameter a string and returns a dictionary
+# that tabulates how many of each letter is in that string.
+
+def count_letters(words):
+    """A functon that counts the number of each letter in a string"""
+    letter_count = {}
+
+    for letter in words:
+        letter = letter.upper()
+        if letter not in letter_count:
+            letter_count[letter] = 1
+        else:
+            letter_count[letter] += 1
+    return letter_count
